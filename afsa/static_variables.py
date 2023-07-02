a2febe1b67d1781fa772969adf3288d1062d37a4
izuku-franck1555 <43177617+izuku-franck1555@users.noreply.github.com>
2023-07-02 13:40:13 +0300

    Update static variables for the workstation

--- afsa/static_variables.py
+++ afsa/static_variables.py
@@ -1,8 +1,8 @@
 import os
 
-TMP_DIRECTORY = "/Users/francktonle/Downloads"
-TIFS_DIRECTORY = "/Users/francktonle/Downloads/OTHERS/Tifs2"
-RESULTS_DIRECTORY = "/Users/francktonle/Documents/WORKSPACE/PYTHON-CCAFS/results"
+TMP_DIRECTORY = "/home/tofrano/Downloads"
+TIFS_DIRECTORY = "/home/tofrano/Documents/WORKSPACE/Franck/Tifs2"
+RESULTS_DIRECTORY = "/home/tofrano/Documents/WORKSPACE/PYTHON-CCAFS/results"
 NORMALIZATION_COEFFICIENTS = {
     'tmean': 10, 'prec': 50, 'tmin': 10, 'tmax': 10, 'bio_1': 10, 'bio_2': 10, 'bio_3': 10, 'bio_4': 10, 'bio_5': 10,
     'bio_6': 10, 'bio_7': 10, 'bio_8': 10, 'bio_9': 10, 'bio_10': 10, 'bio_11': 10, 'bio_12': 10, 'bio_13': 10,
