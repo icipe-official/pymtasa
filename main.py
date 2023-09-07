32ec549097e0e88812f4e35701208b7cc0a53781
FranckMight1555 <francktonlebruno@gmail.com>
2023-09-07 12:05:06 +0300

    Rename AFSA to MTASA

--- main.py
+++ main.py
@@ -7,14 +7,14 @@ import time
 if __name__ == '__main__':
     start_time = time.time()
     parameters = ParametersSet(
-        longitude=-2.1949567,
-        latitude=7.31909,
-        env_vars=("prec", "tmean", "rsds", "wdsp"),
-        weights=(0.25, 0.35, 0.2, 0.2),
-        number_divisions=(12, 12, 12, 12),
+        longitude=36.6790368,
+        latitude=-7.5770145,
+        env_vars=("prec", "tmean"),
+        weights=(0.5, 0.5),
+        number_divisions=(12, 12),
         env_data_ref=RASTER_STACK_ENV_DATA,
         env_data_target=RASTER_STACK_ENV_DATA,
-        analysis_period=[8, 12],
+        analysis_period=[1, 12],
         rotation="both",
         threshold=0,
         rotation_mode=True,
@@ -30,4 +30,4 @@ if __name__ == '__main__':
 
     print("Overall Process : ----%.2f----" % (time.time() - start_time))
 
-    # Utils.plot_raster_file(raster_file_path, parameters.longitude, parameters.latitude)
+    Utils.plot_raster_file(raster_file_path, parameters.longitude, parameters.latitude)
